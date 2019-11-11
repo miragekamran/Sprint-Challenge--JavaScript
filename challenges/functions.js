e1a138c405a4be3820baeab2d05c75ef9c4d7cd3
Mirage Kamran <mirage.kamran.pkt@gmail.com>
2019-11-10 16:38:13 -0800

    sprint updated

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,12 +7,29 @@
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
 
+let consume = (a, b, cb) => {
+  console.log("result: ", cb(a, b));
+}
+
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+
+let add = (num1, num2) => {
+  return num1 + num2;
+}
+
+let multiply = (num1, num2) => {
+  return num1 * num2;
+}
+
+let greeting = (first_name, last_name) => {
+  console.log(`Hello ${first_name} ${last_name} nice to meet you!`);
+}
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
